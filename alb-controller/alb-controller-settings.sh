b8bf8a9449c7ab85e245c6940909337490352c37
ParkJoonTae <ec2-user@ip-10-0-101-86.ap-northeast-2.compute.internal>
2024-06-14 06:50:17 +0000

    alb ingress

--- alb-controller/alb-controller-settings.sh
+++ alb-controller/alb-controller-settings.sh
@@ -1,5 +1,5 @@
 # 하단 명령어 실행 전 alb controller policy 생성 할 것
-# cluster-name: cluster name of eks
+# cluster-name은 자신이 사용중인 cluster-name을 입력
 # policy-arn: alb-controller-policy arn you made by terraform code
 eksctl create iamserviceaccount \
   --name alb-iamserviceaccount \
@@ -11,14 +11,11 @@ eksctl create iamserviceaccount \
 
 # Helm Chart를 이용하여 alb-ingress-controller를 배포
 # cluster-name은 자신이 사용중인 cluster-name을 입력
-# 적용 전에 alb-controller의 버전과 eks 버전의 호환성을 확인 (eks 1.29 <-> alb v2.7.2)
 helm repo add eks https://aws.github.io/eks-charts
 helm repo update eks
 helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
      --set clusterName=pjt-eks \
      --set serviceAccount.create=false \
      --set serviceAccount.name=alb-iamserviceaccount \
-     --set image.repository=amazon/aws-alb-ingress-controller \
-     --set image.tag=v2.7.2 \
      --set extraArgs={--cloud-provider=aws} \
      -n kube-system
