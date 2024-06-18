5274b14fe7655dc471b80e2a4f660fef283debdd
ParkJoonTae <ec2-user@ip-172-31-1-155.ap-northeast-2.compute.internal>
2024-06-18 01:05:07 +0000

    eks version 1.30

--- eks-tf/eks-cluster.tf
+++ eks-tf/eks-cluster.tf
@@ -28,7 +28,7 @@ module "eks" {
   version = ">= 19.0"
 
   cluster_name    = "pjt-eks"
-  cluster_version = 1.29
+  cluster_version = 1.30
 
   vpc_id                   = module.vpc.vpc_id
   subnet_ids               = module.vpc.private_subnets
